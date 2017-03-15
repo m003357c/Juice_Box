ad698066b9fc0dbc60871a744864f39233e620f0
m003357c <m003357c@student.staffs.ac.uk>
2017-03-15 11:56:17 +0000

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,30 @@
+var CACHE_NAME = 'gih-cache';
+var CACHED_URLS = [
+  '../index.html',
+  '../basket.html',
+  '../checkout.html',
+  '../order-status.html',
+  '../css/styles.css',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('index.html');
+        }
+      });
+    })
+  );
+});
