8cb802ec89a65fb830d40e98817fa0add62ac036
m003357c <m003357c@student.staffs.ac.uk>
2017-03-17 16:06:22 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 //JS file for Juice Box
+var citrusPunch = {
+  name:"Citrus Punch",
+  image:"assets/images/basket-citrus.svg",
+  ingredients:"Grapefruit, Orange &amp; Pineapple",
+  price:"	&pound;2.50",
+  className:"citrus-blast"
+};
+var berryBlast = {
+  name:"Berry Blast",
+  image:"assets/images/basket-berry.svg",
+  ingredients:"Apple, Grape, Blueberry &amp; Raspberry",
+  price:"	&pound;2.50",
+  className:"berry-blast"
+};
+var greenMachine = {
+  name:"Berry Blast",
+  image:"assets/images/basket-green.svg",
+  ingredients:"Apple, Banana, Avocado, Kiwi &amp; Lime",
+  price:"	&pound;2.50",
+  className:"berry-blast"
+};
+var simplyRed = {
+  name:"Simply Red",
+  image:"assets/images/basket-red.svg",
+  ingredients:"Apple, Grape, Strawberry &amp; Cherry",
+  price:"	&pound;2.50",
+  className:"simply-red"
+};
+
+var basket = [greenMachine, berryBlast];
+
+console.log(basket);
