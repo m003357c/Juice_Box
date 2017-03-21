679724195d6ef3e215650cf52e926a4d00a53b17
m003357c <m003357c@student.staffs.ac.uk>
2017-03-21 19:05:37 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,97 @@
 //JS file for Juice Box
+var citrusPunch = {
+    name:"Citrus Punch",
+    image:"citrus",
+    ingredients:"Grapefruit, Orange &amp; Pineapple",
+    description:"Grapefruit, Orange &amp; Pineapple",
+    price:"&pound;2.50",
+    drinkClass:"citrus-blast"
+};
+var berryBlast = {
+    name:"Berry Blast",
+    image:"berry",
+    ingredients:"Apple, Grape, Blueberry<br>&amp; Raspberry",
+    description:"Apple, Grape, Blueberry &amp; Raspberry",
+    price:"&pound;2.50",
+    drinkClass:"berry-blast"
+};
+var greenMachine = {
+    name:"Green Machine",
+    image:"green",
+    ingredients:"Apple, Banana, Avocado,<br>Kiwi &amp; Lime",
+    description:"Apple, Banana, Avocado, Kiwi &amp; Lime",
+    price:"&pound;2.50",
+    drinkClass:"green-machine"
+};
+var simplyRed = {
+    name:"Simply Red",
+    image:"red",
+    ingredients:"Apple, Grape, Strawberry<br>&amp; Cherry",
+    description:"Apple, Grape, Strawberry &amp; Cherry",
+    price:"&pound;2.50",
+    drinkClass:"simply-red"
+};
+
+var drinks = [citrusPunch, greenMachine, berryBlast, simplyRed];
+
 var drinkClicked = document.getElementsByClassName("drink-option");
 
-var showDrinkInfo = function() {
-    var drinkID = this.id;
+function drinkInfoTemplate(){
     
+  var drinkName = item.name,
+      drinkImage = item.image,
+      drinkDesc = item.description,
+      drinkPrice = item.price,
+      drinkClass = item.drinkClass;      
+  
+  var template =    '<article class="product-information ' + drinkClass + '">' +
+                    '<img src="assets/images/prod-' + drinkImage + '.svg" alt="' + drinkName + ' ingredient cup">' +
+                    '<p class="btn-holder"><a href="#" class="btn add-cart-btn">Add to<br>cart</a><a href="#" class="btn buy-now-btn">Vend<br>now</a></p>' +
+                    '<div class="info-holder">' +
+                       '<h2>' + drinkName + ' - ' + drinkPrice + '</h2>' +
+                        '<p class="product-info">' + drinkDesc + '</p>' +
+                        '<p class="nurti-prompt">^ Scroll for nutrition &amp; allergy info ^</p>' +
+                        '<div class="nutri-table">' +
+                            '<table>' +
+                               ' <tbody><tr>' +
+                                   '<th></th>' +
+                                   '<th>per serving (250ml)</th>' +
+                               '</tr>' +
+                                '<tr>' +
+                                    '<td>Energy</td>' +
+                                    '<td>567.5kj (135.6kcal)</td>' +
+                                '</tr>' +
+                                '<tr>' +
+                                    '<td>Sugar</td>' +
+                                    '<td>27g</td>' +
+                                '</tr>' +
+                                '<tr>' +
+                                    '<td>Fat</td>' +
+                                    '<td>0g</td>' +
+                                '</tr>' +
+                                '<tr>' +
+                                    '<td>Carbohydrates</td>' +
+                                    '<td>30g</td>' +
+                                '</tr>' +
+                                '<tr>' +
+                                    '<td>Fibre</td>' +
+                                    '<td>3.5g</td>' +
+                                '</tr>' +
+                            '</tbody></table>' +
+                        '</div>' +
+                    '</div>' +
+                '</article>';
+  
+  var target = document.getElementById("basket");
+  
+  target.innerHTML = target.innerHTML + template;
+    
+}
+
+var showDrinkInfo = function() {
+    
+    var drinkID = this.id;
+   
     switch (drinkID) {
         case "citrus":
             var citrusId = document.getElementById("citrusProdInfo");
@@ -33,34 +121,7 @@ for (var i = 0; i < drinkClicked.length; i++) {
 }
 
 
-var citrusPunch = {
-  name:"Citrus Punch",
-  image:"citrus",
-  ingredients:"Grapefruit, Orange &amp; Pineapple",
-  price:"&pound;2.50",
-  drinkClass:"citrus-blast"
-};
-var berryBlast = {
-  name:"Berry Blast",
-  image:"berry",
-  ingredients:"Apple, Grape, Blueberry<br>&amp; Raspberry",
-  price:"&pound;2.50",
-  drinkClass:"berry-blast"
-};
-var greenMachine = {
-  name:"Green Machine",
-  image:"green",
-  ingredients:"Apple, Banana, Avocado,<br>Kiwi &amp; Lime",
-  price:"&pound;2.50",
-  drinkClass:"green-machine"
-};
-var simplyRed = {
-  name:"Simply Red",
-  image:"red",
-  ingredients:"Apple, Grape, Strawberry<br>&amp; Cherry",
-  price:"&pound;2.50",
-  drinkClass:"simply-red"
-};
+
 
 var basket = [greenMachine, berryBlast];
 
