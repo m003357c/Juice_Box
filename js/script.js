bb5a6a9e18e3f8924f60013cb2146fc2cd8a0534
m003357c <m003357c@student.staffs.ac.uk>
2017-03-22 17:03:56 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
 		}, 150 * (i + 1));
 	});
 	
-	$(".basket-item .remove-basket").on("click", ".basket-item", function(){
+	$(".basket-item .remove-basket").on("click", ".remove-basket", function(){
 		console.log("click");
 		$(this).parent().removeClass("is-showing");
 		setTimeout(function(){
