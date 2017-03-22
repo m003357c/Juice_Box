d03b6d6404d465e45c876563f4446b624700110e
m003357c <m003357c@student.staffs.ac.uk>
2017-03-22 17:25:39 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -108,13 +108,15 @@ $(document).ready(function(){
 	$(document).on("click", ".remove-basket", function(){
 		console.log("click");
 		$(this).parent().removeClass("is-showing");
-		$(this).closest(".basket-item").remove();
-		
+
 		setTimeout(function(){
 			console.log("boom");
 			
 			console.log("bam");
 		}, 300);
+		setTimeout(function(){
+			$(this).closest(".basket-item").remove();
+		}, 5000);
 	});
 	
 	
