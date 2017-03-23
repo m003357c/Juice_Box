952f5c04ccc73628d7f5dca690fabc972c1a74f1
m003357c <m003357c@student.staffs.ac.uk>
2017-03-23 20:02:07 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -160,23 +160,23 @@ $(document).ready(function(){
 	
 	$(".pay-screen .btn").click(function(){		
 		$(".overlay").toggleClass("fade fadeOut").delay(1000).queue(function(){ $(this).removeClass("isShowing fadeOut"); } );
-		setTimeout(function(){
-			if ($(".card.animated").hasClass("mobile")){ 
-				$(".card.mobile").toggleClass("mobile-animate-in mobile-animate-out").delay(1000).queue(function() { $(this).removeClass("mobile-animate-out").css("z-index","1");  } );	
+		
+		if ($(".card.animated").hasClass("mobile")){ 
+			$(".card.mobile").toggleClass("mobile-animate-in mobile-animate-out").delay(1000).queue(function() { $(this).removeClass("mobile-animate-out").css("z-index","1");  } );	
 
-			}else if ($(".card").hasClass("debit")){
+		}else if ($(".card").hasClass("debit")){
 
-				$(".card.debit").addClass("card-animate-in card-animate-out").delay(1000).queue(function() { $(this).removeClass("card-animate-out").css("z-index","2"); } );	
+			$(".card.debit").addClass("card-animate-in card-animate-out").delay(1000).queue(function() { $(this).removeClass("card-animate-out").css("z-index","2"); } );	
 
-			} else if ($(".card").hasClass("paypal")){
+		} else if ($(".card").hasClass("paypal")){
 
-				$(".card.paypal").addClass("paypal-animate-in paypal-animate-out").delay(1000).queue(function() { $(this).removeClass("paypal-animate-out").css("z-index","3"); } );	
+			$(".card.paypal").addClass("paypal-animate-in paypal-animate-out").delay(1000).queue(function() { $(this).removeClass("paypal-animate-out").css("z-index","3"); } );	
 
-			};
+		};
 
-			$(".wallet-inner").css("position","relative");		
-			$(".card-inner").removeClass("fade");
-		}, 500);
+		$(".wallet-inner").css("position","relative");		
+		$(".card-inner").removeClass("fade");
+
 		
 		
 	});
