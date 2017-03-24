d5eb905649374916f893712465220b7400e8d807
m003357c <m003357c@student.staffs.ac.uk>
2017-03-24 15:52:00 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -190,8 +190,8 @@ $(document).ready(function(){
 				$(".wallet-inner").css("position","relative");		
 				$(".card-inner").removeClass("fade");
 			}else if ($(".cash").hasClass("animated")){
-				$(".cash").toggleClass("cash-animate-in cash-animate-out").delay(1000).queue(function(next) {
-					$(".cash").attr("class","cash").attr("style","");
+				$(".cash").toggleClass("cash-animate-in cash-animate-out").attr("style","").delay(1000).queue(function(next) {
+					$(".cash").attr("class","cash");
 					next();
 				});
 				$(".wallet-inner").css("position","relative");	
