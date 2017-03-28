07d3547380da3a752ad07ba11600c6cbd5e1e6ce
m003357c <m003357c@student.staffs.ac.uk>
2017-03-28 16:51:33 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,13 +1,18 @@
-var CACHE_NAME = 'Juice_Box_Offline_Pages';
+var CACHE_NAME = 'JuiceBoxCache';
+var BASEPATH = '/Juice_Box/';
 var CACHED_URLS = [
-  '../offline-index.html',
-  '../offline-basket.html',
-  ' ../offline-checkout.html',
-  '../css/styles.css',
-  '../js/script.js'
+  // Our HTML
+    BASEPATH + 'index.html',
+  // Stylesheets and fonts    
+    BASEPATH +  '/css/styles.css',
+  // JavaScript
+    BASEPATH +  'manifest.json',
+    BASEPATH +  'js/script.js',
+  // Images
 ];
 
 self.addEventListener('install', function(event) {
+  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
@@ -16,15 +21,43 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('offline-index.html');
-        }
-      });
+  var requestURL = new URL(event.request.url);
+  if (requestURL.pathname === BASEPATH + 'index.html') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match('index.html').then(function(cachedResponse) {
+          var fetchPromise = fetch('index.html').then(function(networkResponse) {
+            cache.put('index.html', networkResponse.clone());
+            return networkResponse;
+          });
+          return cachedResponse || fetchPromise;
+        });
+      })
+    );
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        })
+      })
+    );
+  }
+});
+
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('JuiceBoxCache') && CACHE_NAME !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
