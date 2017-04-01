ed9562118286ac3e6cd03c79927137f73cb161c4
m003357c <m003357c@student.staffs.ac.uk>
2017-04-01 14:18:22 +0100

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,65 @@
+var CACHE_NAME = 'JuiceBoxCache';
+var BASEPATH = '/Juice_Box/';
+var CACHED_URLS = [
+  // Our HTML
+    BASEPATH + 'index.html',
+    BASEPATH + 'basket.html',
+    BASEPATH + 'checkout.html',
+  // Stylesheets and fonts    
+    BASEPATH +  'css/styles.css',
+  // JavaScript
+    BASEPATH +  'assets/icons/manifest.json',
+    BASEPATH +  'js/script.js'
+  // Images
+];
+
+self.addEventListener('install', function(event) {
+  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestURL = new URL(event.request.url);
+  if (requestURL.pathname === BASEPATH + 'index.html') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match('index.html').then(function(cachedResponse) {
+          var fetchPromise = fetch('index.html').then(function(networkResponse) {
+            cache.put('index.html', networkResponse.clone());
+            return networkResponse;
+          });
+          return cachedResponse || fetchPromise;
+        });
+      })
+    );
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        })
+      })
+    );
+  }
+});
+
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('JuiceBoxCache') && CACHE_NAME !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
